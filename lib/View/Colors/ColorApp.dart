f60d48183a36c878de226b9b3bb85c4dcd0ebf14
Ahmed Shaker <amhmeed31@gmail.com>
2024-06-16 15:34:18 +0300

    complate dropdownbutton

---
+++ lib/View/Colors/ColorApp.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ColorApp {
+  static const Color graylight = Color.fromARGB(255, 215, 215, 215);
+}
--- lib/View/HomePage/HomePage.dart
+++ lib/View/HomePage/HomePage.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:guide_of_iraq/Control/ShowDataController.dart';
+import 'package:guide_of_iraq/View/Colors/ColorApp.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -14,13 +18,28 @@ class HomePage extends StatelessWidget {
           body: SafeArea(
               child: Column(
             children: [
+              const SizedBox(
+                height: 10.0,
+              ),
               Expanded(
                 child: Row(
                   children: [
-                    Expanded(
-                      flex: 1,
+                    const SizedBox(
+                      width: 8.0,
+                    ),
+                    Container(
+                      // color: ColorApp.graylight,
+                      decoration: BoxDecoration(
+                        color: ColorApp.graylight, // Background color
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.black, // Border color
+                          width: 1.0, // Border width
+                        ),
+                      ),
                       child: IconButton(
                         onPressed: () {},
+                        iconSize: 35.0,
                         icon: const Icon(
                           Icons.notification_add,
                         ),
@@ -31,36 +50,74 @@ class HomePage extends StatelessWidget {
                     ),
                     Expanded(
                       flex: 4,
-                      child: TextFormField(),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: ColorApp.graylight, // Background color
+                          hintText: 'Search'.tr,
+                          hintStyle: const TextStyle(color: Colors.black),
+                          border: const OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(8.0)),
+                          ),
+                          labelText: "Search".tr,
+
+                          prefixIcon: const Icon(
+                            Icons.search,
+                            color: Colors.black,
+                          ),
+                          // border: OutlineInputBorder(),
+                          focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black,
+                            ),
+                          ),
+                          labelStyle: const TextStyle(
+                            color: Colors.black,
+                            //  background: ,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 8.0,
                     ),
                   ],
                 ),
+              ),
+              const SizedBox(
+                height: 8.0,
               ),
               Expanded(
                 flex: 1,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: OutlinedButton(
-                        onPressed: () {},
-                        child: const Text('data'),
-                      ),
+                    const SizedBox(
+                      height: 4.0,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: OutlinedButton(
-                        onPressed: () {},
-                        child: const Text('data'),
-                      ),
+                    DropdownExample(
+                      name: "Doctor",
+                      dropdownItems: const [
+                        'Lawer',
+                        'Doctor',
+                        'Pharmacal',
+                      ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: OutlinedButton(
-                        onPressed: () {},
-                        child: const Text('data'),
-                      ),
+                    DropdownExample(
+                      name: "Eye surgery",
+                      dropdownItems: const [
+                        "Eye surgery",
+                      ],
+                    ),
+                    DropdownExample(
+                      name: "Baghdad",
+                      dropdownItems: const [
+                        'Baghdad',
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 4.0,
                     ),
                   ],
                 ),
@@ -70,7 +127,6 @@ class HomePage extends StatelessWidget {
                 child: ListView.builder(
                   itemCount: 5,
                   itemBuilder: (context, i) {
-                    
                     return const ListTile(
                       leading: Text(
                         'Ali',
@@ -88,6 +144,55 @@ class HomePage extends StatelessWidget {
           )),
         );
       },
+    );
+  }
+}
+
+class DropdownExample extends StatefulWidget {
+  DropdownExample({required this.name, required this.dropdownItems, super.key});
+  String? name;
+  List<String>? dropdownItems;
+  @override
+  _DropdownExampleState createState() => _DropdownExampleState();
+}
+
+class _DropdownExampleState extends State<DropdownExample> {
+  String? _selectedValue;
+
+  List<String>? _dropdownItems;
+  @override
+  void initState() {
+    super.initState();
+    _selectedValue = widget.name;
+    // Initialize state with the constructor parameter
+    _dropdownItems = widget.dropdownItems;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(4.0),
+      decoration: BoxDecoration(
+        color: ColorApp.graylight, // Background color
+        borderRadius: BorderRadius.circular(6),
+        border: Border.all(color: Colors.black, width: 2),
+      ),
+      child: DropdownButton<String>(
+        dropdownColor: ColorApp.graylight,
+        hint: Text(_selectedValue!),
+        value: _selectedValue,
+        items: _dropdownItems!.map((String item) {
+          return DropdownMenuItem<String>(
+            value: item,
+            child: Text(item),
+          );
+        }).toList(),
+        onChanged: (String? newValue) {
+          setState(() {
+            _selectedValue = newValue;
+          });
+        },
+      ),
     );
   }
 }
